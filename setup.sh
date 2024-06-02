cc54be07248e663c889219d615a68a10676e79b8
Orad Segal <124451776+7Pawns@users.noreply.github.com>
2024-06-02 20:14:05 +0300

    add install reqs in setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,9 @@
 # OpenSSL key and cert creation
 openssl req -x509 -newkey rsa:4096 -nodes -out cert.pem -keyout key.pem -days 365
 
+# Download python requirements
+python -m pip install -r requirements.txt
+
 # Flask setup
 echo "FLASK_SECRET_KEY=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)" > api/.env
 
