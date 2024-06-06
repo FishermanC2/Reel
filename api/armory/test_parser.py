9fd7a41c73595ac1f4ebef5f03fc4af0f42a870a
Orad Segal <124451776+7Pawns@users.noreply.github.com>
2024-06-06 05:41:17 +0300

    fix tests

--- api/armory/test_parser.py
+++ api/armory/test_parser.py
@@ -4,7 +4,9 @@ import base64
 
 class TestBaitParser(unittest.TestCase):
     def test_default_bait_parse(self):
-        encoded_bait = base64.b64decode(Bait.b64_encode("fetch.js").encode()).decode()
+        encoded_bait = base64.b64decode(Bait.b64_encode("fetch.js")
+                                        .replace("<script>eval(atob(\'", "")
+                                        .replace("\'));</script>", "").encode()).decode()
         self.assertNotEqual(encoded_bait.find('127.0.0.1:5000'), -1)
         self.assertNotEqual(encoded_bait.find('http'), -1)
 
